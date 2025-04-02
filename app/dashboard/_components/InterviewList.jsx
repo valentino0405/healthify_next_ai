e8ae4a6db9808c1fbff848ffa33a8fa6002fe95f
valentino0405 <155987346+valentino0405@users.noreply.github.com>
2025-04-02 14:59:43 +0530

    Update InterviewList.jsx

--- app/dashboard/_components/InterviewList.jsx
+++ app/dashboard/_components/InterviewList.jsx
@@ -30,7 +30,7 @@ function InterviewList() {
 
     return (
         <div>
-            <h2 className='font-medium text-xl'>Previous Mock Interview</h2>
+            <h2 className='font-medium text-xl'>Previous Health Problems</h2>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-3'>
                 {interviewList && interviewList.map((interview, index) => (
                             <InterviewItemCard 
